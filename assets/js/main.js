bc65bb605ba6d45a0d17fc6f46e8d8910acdc2c8
Mariano Inostroza <mariano.f.inostroza@gmail.com>
2026-02-09 22:34:45 -0300

    Se crean archivos fundamentales

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+class Tarea {
+    constructor(id, descripcion, estado, fechaCreacion) {
+    this.id = id;
+    this.descripcion = descripcion;
+    this.estado = estado;
+    this.fechaCreacion = fechaCreacion;
+    }
+}
+
+class GestorTareas {
+
+}
+
+

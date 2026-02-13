579dd880b7ce0692e9c395cc1e543c2e67a279f9
Mariano Inostroza <mariano.f.inostroza@gmail.com>
2026-02-12 21:02:58 -0300

    se añaden funciones a main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
-let listaTareas []
+let listaTareas = []
+
+const nombreTarea = document.getElementById(nombreTarea)
+const descripcion = document.getElementById(descripcion)
+const fechaLimite = document.getElementById(fechaLimite)
 
 class Tarea {
-    constructor(id, descripcion, estado, fechaCreacion) {
+    constructor(id, descripcion, estado, fechaCreacion, fechaLimite) {
     this.id = id;
     this.descripcion = descripcion;
     this.estado = estado;
     this.fechaCreacion = fechaCreacion;
+    this.fechaLimite = fechaLimite;
     }
 }
 
@@ -13,6 +18,14 @@ class GestorTareas {
 
 }
 
+function toggleCompletar() {
+
+}
 
 
+function normalizarTexto(texto){
+    return String(texto)
+        .trim()
+        .replace(/\s+/g , " ")
+}
 
